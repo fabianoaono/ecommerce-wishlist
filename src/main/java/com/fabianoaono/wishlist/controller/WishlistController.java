e532c041a544e13789c97a709405aa5dc4be47bc
Fabiano Aono <fabianoaono@gmail.com>
2023-11-13 21:10:04 -0300

    Inclusion of api version

--- src/main/java/com/fabianoaono/wishlist/controller/WishlistController.java
+++ src/main/java/com/fabianoaono/wishlist/controller/WishlistController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/wishlist")
+@RequestMapping("/api/v1/wishlist")
 public class WishlistController {
 
     @Autowired
